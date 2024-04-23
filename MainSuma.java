7f4c98cfef6b03eb19c6c58e7ee9490e47701ade
unknown <jgonzalo.hernandezy@gmail.com>
2024-04-23 00:46:52 -0600

    Suma de 2 numeros p1

---
+++ MainSuma.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class MainSuma{
+        public static void main(String[] args) {
+        Scanner n1 = new Scanner(System.in);
+     System.out.println("Ingresa un numero");
+        int a=n1.nextInt();
+        System.out.println("Ingrese el segundo numero");
+        int b=n1.nextInt();
+        int r=a+b;
+        System.out.println("El resultado es:"+r);
+        
+        
+    }
+}
---
+++ Suma.java
@@ -0,0 +1,7 @@
+public class Suma {
+    int a, b, r;
+    public int sumar(int a, int b) {
+        r = this.a + this.b;
+        return r;
+    }
+}
